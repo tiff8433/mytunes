5a2cca9d14bb3e263cf6ec07af6cb3052f83227a
tiff8433 <tiffany.l.huang@gmail.com>
2015-12-11 18:34:08 -0800

    Completes basic requirements

--- client/collections/SongQueue.js
+++ client/collections/SongQueue.js
@@ -4,6 +4,7 @@ var SongQueue = Songs.extend({
   initialize: function(){
     this.on('add', this.enqueue, this);
     this.on('ended', this.playNext, this);
+    this.on('dequeue', this. dequeue, this);
   },
 
   enqueue: function(song){
@@ -24,6 +25,14 @@ var SongQueue = Songs.extend({
       this.playFirst();
     } else {
       this.trigger('stop');
+    }
+  },
+
+  dequeue: function(song){
+    if(this.at(0) === song){
+      this.playNext();
+    } else {
+      this.remove(song);
     }
   }
 
